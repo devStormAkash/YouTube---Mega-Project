a3868bc30eda05f78629b0a91ba5721456c39068
devStormAkash <iitjee523@gmail.com>
2025-06-12 07:00:54 +0530

    add getUserChannelProfile function to fetch user profile with subscription details

--- BackEnd/src/controllers/user.controller.js
+++ BackEnd/src/controllers/user.controller.js
@@ -320,6 +320,8 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200,userAfterUpdatingAvatar,"Avatar updated successfully" ))
 })
 
+// delete the previos photo which is already uploaded in the cloudinary
+
 const updateUserCoverImage = asyncHandler(async (req, res) => {
   const coverImageLocalPath = req.file?.path;
   if (!coverImageLocalPath) {
@@ -354,6 +356,70 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     );
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params 
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "User not exist")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: { username: username?.toLowerCase() },
+        },
+
+        {
+            $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers",
+            },
+        },
+        {
+            $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo",
+            },
+        },
+        {
+            $addFields: {
+                subscriberCounts: {
+                  $size : "$subscribers"
+                },
+                channelsSubscriberCounts: {
+                    $size : "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else : false
+                    }
+                }
+          }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                email: 1,
+                avatar: 1,
+                coverImage: 1,
+                subscriberCounts: 1,
+                channelsSubscriberCounts: 1,
+                isSubscribed : 1 
+            }
+        }   
+    ]);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, channel[0], "Profile fetched successfully"));
+})
+
 
 export {
     registerUser,
@@ -364,5 +430,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 };
